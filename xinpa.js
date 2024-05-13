fbbca54713b77f8d8fd8331af8376e7745f99b7a
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-05-13 21:35:22 +0800

    Update xinpa.js

--- xinpa.js
+++ xinpa.js
@@ -1,22 +1,29 @@
-/// 定义请求的 URL
+// 定义请求的 URL
 const apiUrl = "https://3650000.xyz/api";
 
 // 发送 HTTP 请求
-$httpClient.get(apiUrl, function(error, response, data) {
-  if (error) {
-    // 发生错误时，输出错误信息
-    console.log("Error: " + error);
-  } else {
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
     // 解析返回的数据
-    const responseData = JSON.parse(data);
-
-    // 获取图片链接
-    const imageUrl = responseData.url;
+    const imageUrl = data.url;
 
     // 输出图片链接
     console.log("Image URL: " + imageUrl);
 
-    // 在 Quantumult X 上展示通知，并可以点击打开图片链接
-    $notification.post("图片链接获取完成", "", "点击查看图片", imageUrl);
-  }
-});
+    // 在浏览器中展示通知，并可以点击打开图片链接
+    Notification.requestPermission().then(function(result) {
+      if (result === 'granted') {
+        var notification = new Notification('图片链接获取完成', {
+          body: '点击查看图片',
+          image: imageUrl
+        });
+        notification.onclick = function(event) {
+          window.open(imageUrl, '_blank');
+        }
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
