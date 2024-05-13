7ab8b0c76c99c97613676d158455d4945386869f
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-05-13 21:15:43 +0800

    Update xinpa.js

--- xinpa.js
+++ xinpa.js
@@ -7,39 +7,35 @@ const url = "https://www.meizi5.com/";
 $task.fetch({
   url: url,
   method: "GET",
-}).then(
-  response => {
-    // 响应成功
-    const data = response.body;
-    // 使用正则表达式匹配所有图片的 src 属性
-    const regex = /<img.*?src="(.*?)".*?>/g;
-    let match;
-    const images = [];
+}).then(response => {
+  // 响应成功
+  const data = response.body;
+  // 使用正则表达式匹配所有图片的 src 属性
+  const regex = /<img.*?src="(.*?)".*?>/g;
+  let match;
+  const images = [];
 
-    // 循环匹配所有图片链接
-    while ((match = regex.exec(data)) !== null) {
-      // 添加图片链接到数组
-      images.push(match[1]);
-    }
-
-    // 检查是否找到图片
-    if (images.length > 0) {
-      // 选择第一张图片进行展示
-      const firstImage = images[0];
-
-      // 在Quantumult X中展示通知
-      $notification.post("图片获取成功", "", "第一张图片: " + firstImage);
-      // 这里可以添加更多的逻辑来处理其他图片
-    } else {
-      $notification.post("图片获取失败", "", "没有找到图片链接");
-    }
-
-    // 输出所有找到的图片链接
-    console.log("Found images: " + images.join(", "));
-  },
-  error => {
-    // 请求失败
-    console.log("HTTP request failed: ", error);
-    $notification.post("图片获取失败", "", "HTTP请求失败");
+  // 循环匹配所有图片链接
+  while ((match = regex.exec(data)) !== null) {
+    // 添加图片链接到数组
+    images.push(match[1]);
   }
-);
+
+  // 检查是否找到图片
+  if (images.length > 0) {
+    // 选择第一张图片进行展示
+    const firstImage = images[0];
+
+    // 在Quantumult X中展示通知
+    $notification.post("图片获取成功", "", "第一张图片: " + firstImage);
+    // 这里可以添加更多的逻辑来处理其他图片
+  } else {
+    $notification.post("图片获取失败", "", "没有找到图片链接");
+  }
+
+  // 输出所有找到的图片链接
+  console.log("Found images: " + images.join(", "));
+}, reason => {
+  // 请求失败
+  console.log("Error: " + reason.error);
+});
