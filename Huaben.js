527a1434cd1af500987ee4ac38fa69ed809d6669
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-07-12 18:02:19 +0800

    Update Huaben.js

--- Huaben.js
+++ Huaben.js
@@ -1,8 +1,14 @@
+/******************************
+脚本功能：话本小说-去广告，解锁本地会员标识
+脚本作者：Yue
+更新时间：2024-07-12
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
 [rewrite_local]
-^https:\/\/user\.ihuaben\.com\/ url script-response-body https://github.com/Yuehuaer/QX/blob/main/Huaben.js
+^https:\/\/user\.ihuaben\.com\/ url script-response-body https://raw.githubusercontent.com/Yuehuaer/QX/main/huaben.js
 [mitm] 
 hostname = user.ihuaben.com
-
+*******************************/
 
 var Yue = $response.body;
 var obj =  JSON.parse(Yue);
@@ -14,3 +20,6 @@ if($request.url.indexOf("/api/userinfo") != -1) {
 
 Yue = JSON.stringify(obj);
 $done(Yue);
+
+
+
