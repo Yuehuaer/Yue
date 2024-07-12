696b8836cbea5ccf9939ee409504f32146257c37
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-07-12 18:11:26 +0800

    Update ce1.js

--- ce1.js
+++ ce1.js
@@ -11,7 +11,7 @@ hostname = api.iyykj.cn
 *******************************/
 var Yue = $response.body;
 var obj =  JSON.parse(Yue);
-if($request.url.indexOf("/api/app/user/info") != -1) {
+if($request.url.indexOf("/api/app/user/info") != -1) 
  obj={
    "code" : 200,
   "data" : {
@@ -19,20 +19,16 @@ if($request.url.indexOf("/api/app/user/info") != -1) {
     "vipEndTime" : 2099-01-01T00:00:00,
     "inviteCode" : "SZRGUB4V",
     "gmtCreate" : "2024-07-12T16:19:52",
-    "userName" : "bl0104882",
+    "userName" : "Yuehua",
     "userStatus" : 1,
     "vipFlag" : 1,
     "nickName" : "Yue",
     "avatar" : null,
     "invitedFlag" : true,
-    "goldNum" : 0,
+    "goldNum" : 1,
     "vipType" : 1,
-    "vipStartTime" : 2099-01-01T00:00:00,
+    "vipStartTime" : 2024-07-11T00:00:00,
     "gender" : 0
-  },
-  "msg" : "获取成功",
-  "traceId" : "5c005461-803c-4fad-9031-91c3f8b7bdf4"
- };
 }
 Yue = JSON.stringify(obj);
 $done({body;Yue});
