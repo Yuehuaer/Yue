660c399e551d78b79b5932d0c16ac3820dca1a52
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-07-20 19:17:45 +0800

    更新 ce1.js

--- ce1.js
+++ ce1.js
@@ -5,34 +5,14 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/api\.iyykj\.cn\/ url script-response-body https://raw.githubusercontent.com/Yuehuaer/QX/main/ce1.js
+ ^https:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/Yuehuaer/QX/main/ce1.js
 [mitm] 
-hostname = api.iyykj.cn
+hostname = api.revenuecat.com
 *******************************/
 var Yuer = $response.body;
 var obj =  JSON.parse(Yuer);
 if($request.url.indexOf("/api/app/user/info") != -1)  {
- obj={
-   "code" : 200,
-  "data" : {
-    "beanNum" : 0,
-    "vipEndTime" : null,
-    "inviteCode" : "SZRGUB4V",
-    "gmtCreate" : "2024-07-12T16:19:52",
-    "userName" : "bl0104882",
-    "userStatus" : 1,
-    "vipFlag" : 1,
-    "nickName" : "用户0104882",
-    "avatar" : null,
-    "invitedFlag" : false,
-    "goldNum" : 0,
-    "vipType" : 0,
-    "vipStartTime" : null,
-    "gender" : 0
-  },
-  "msg" : "获取成功",
-  "traceId" : "5c005461-803c-4fad-9031-91c3f8b7bdf4"
- };
+ obj.subscriber.subscriptions.qbit_8_month.is_sandbox = true
 }
 Yuer = JSON.stringify(obj);
 $done({body;Yuer});
