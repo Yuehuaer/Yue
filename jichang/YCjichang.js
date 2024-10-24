a8d0900c16d2c958d4273e2a6d555b53b21a5c76
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-10-24 13:40:12 +0800

    Update YCjichang.js

--- jichang/YCjichang.js
+++ jichang/YCjichang.js
@@ -1,5 +1,6 @@
 # 远程订阅连接-----
 # 网络收集-随意用用
+# 修改时间2024-10-24
 
 #一分机场  2024-11-12到期 1T
 https://webget.yfjc.xyz/api/v1/client/subscribe?token=7a9dd5ea72f7d445ff0e17ef00fb0c0d
@@ -13,8 +14,8 @@ https://newdy2.smjc.top/1145141919810/api/v1/client/subscribe?token=d705e254a114
 #闪云 公益服 无限时间977TB
 https://qq.xlm.plus/api/v1/client/subscribe?token=9e5db95336ab586ac6c4e2306c4d25a9
 
-
-
+#无名 2024-11-08到期 130G
+https://igdux.top/~wuxianliuliang
 
 
 
