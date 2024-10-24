ee273784321f4cda359bccd9d6246d4723bb2e9d
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-10-24 13:35:08 +0800

    Update YCjichang.js

--- jichang/YCjichang.js
+++ jichang/YCjichang.js
@@ -3,3 +3,29 @@
 
 #一分机场  2024-11-12到期
 https://webget.yfjc.xyz/api/v1/client/subscribe?token=7a9dd5ea72f7d445ff0e17ef00fb0c0d
+
+#魔戒  130G 无时间
+https://mojie.app/api/v1/client/subscribe?token=45a10a1849e7d05172fbb0c07a01fdc0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
