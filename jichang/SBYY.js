488c2cf2643a388f8181a71378421b954a4eafbf
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2024-10-30 19:34:18 +0800

    更新 SBYY.js

--- jichang/SBYY.js
+++ jichang/SBYY.js
@@ -1,22 +1,8 @@
 # 远程订阅连接-----
 # 网络收集-随意用用
-# 修改时间2024-10-24
+# 修改时间2024-10-30
 
-#一分机场  2024-11-12到期 1T
-https://webget.yfjc.xyz/api/v1/client/subscribe?token=7a9dd5ea72f7d445ff0e17ef00fb0c0d
-
-#魔戒  130G 无时间
-https://mojie.app/api/v1/client/subscribe?token=45a10a1849e7d05172fbb0c07a01fdc0
-
-#三毛机场 2024-11-12到期 1T
-https://newdy2.smjc.top/1145141919810/api/v1/client/subscribe?token=d705e254a114d7367ae9ed0932281dd9
-
-#闪云 公益服 无限时间977TB
-https://qq.xlm.plus/api/v1/client/subscribe?token=9e5db95336ab586ac6c4e2306c4d25a9
-
-#无名 2024-11-08到期 130G
-https://igdux.top/~wuxianliuliang
-
+https://gbtgurl.me/api/v1/client/subscribe?token=bd0f388767d29819fced466ec0a19675
 
 
 
